c41f1dd14e8d974b6cb3308bec4546d104c64ff0
100ky <stoky911@gmail.com>
2025-09-22 17:01:53 +0000

    aktualizace readme

--- SQL/moje_dotazy.sql
+++ SQL/moje_dotazy.sql
@@ -1,17 +1,6 @@
-SELECT COUNT(rating) FROM film
-WHERE rating <> 'R';
+SELECT staff_id, SUM(amount) FROM payment
+GROUP BY staff_id;
 
-SELECT COUNT(replacement_cost) FROM film
-WHERE replacement_cost >= 15;
 
-SELECT COUNT(rental_rate) FROM film
-WHERE rental_rate >=3 AND rating = 'R';
-
-SELECT last_name, email FROM customer
-WHERE last_name = 'Fisher';
-
-SELECT description FROM film
-WHERE title = 'Blanket Beverly';
-
-SELECT COUNT(length) FROM film
-WHERE length >= 60 AND rating = 'R';
+SELECT COUNT(staff_id) FROM payment
+GROUP BY staff_id;
